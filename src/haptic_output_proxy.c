e6240ffebb0e16ea75088d71e48ab827c6d13875
Oleksandr Beshchuk <bs.alex.mail@gmail.com>
2026-03-22 22:39:29 +0000

    Cleanup unused code

--- src/haptic_output_proxy.c
+++ src/haptic_output_proxy.c
@@ -8,146 +8,23 @@
 
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
-#include <zephyr/drivers/haptics.h>
-#include <zephyr/drivers/haptics/drv2605.h>
-#include <zephyr/logging/log.h>
-#include <zephyr/sys/util.h>
-
-#if IS_ENABLED(CONFIG_ZMK_SPLT_PERIPHERAL_OUTPUT_RELAY) && IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-#include <zmk/split/output-relay/event.h>
-extern int zmk_split_bt_invoke_output(const struct device *dev,
-                                      struct zmk_split_bt_output_relay_event event);
-#endif
-typedef int (*output_set_value_t)(const struct device *dev, uint8_t value);
-typedef int (*output_get_ready_t)(const struct device *dev);
-
-struct output_generic_api {
-    output_set_value_t set_value;
-    output_get_ready_t get_ready;
-};
-
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
-LOG_MODULE_REGISTER(zmk_haptic_output_proxy, CONFIG_ZMK_BEHAVIOR_HAPTIC_DRV2605_LOG_LEVEL);
-
 struct haptic_output_proxy_config {
-    const struct device *output_dev;
+    /* Kept alive solely as a structural pointer link for the Zephyr RTOS split relay */
 };
-
-struct haptic_output_proxy_data {
-    uint8_t pending_val;
-};
-
-static int haptic_output_proxy_start_output(const struct device *dev) {
-    const struct haptic_output_proxy_config *config = dev->config;
-    struct haptic_output_proxy_data *data = dev->data;
-
-    LOG_DBG("Proxy start output with val %d", data->pending_val);
-
-    if (config->output_dev) {
-        const struct output_generic_api *api =
-            (const struct output_generic_api *)config->output_dev->api;
-
-        if (api && api->set_value) {
-            return api->set_value(config->output_dev, data->pending_val);
-        }
-    } else {
-#if IS_ENABLED(CONFIG_ZMK_SPLT_PERIPHERAL_OUTPUT_RELAY) && IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-        struct zmk_split_bt_output_relay_event ev = {
-            .value = data->pending_val,
-        };
-        return zmk_split_bt_invoke_output(dev, ev);
-#else
-        LOG_ERR("No output_dev and split relay disabled on this split role");
-#endif
-    }
-
-    return -ENOTSUP;
-}
-
-static int haptic_output_proxy_stop_output(const struct device *dev) { return 0; }
-
-static const struct haptics_driver_api haptic_output_proxy_api = {
-    .start_output = haptic_output_proxy_start_output,
-    .stop_output = haptic_output_proxy_stop_output,
-};
-
-// The zephyr haptics API uses a union for config_data, but the signature in driver_api might vary
-// slightly affecting strict types. Let's double check standard haptics API. Actually
-// zephyr/drivers/haptics/drv2605.h defines specific config struct, but the driver api is generic.
-// Wait, `drv2605_haptic_config` is a specific API function for DRV2605.
-// `behavior_haptic_drv2605` calls `drv2605_haptic_config` directly, NOT via `haptics_driver_api`.
-// This is a problem. `behavior_haptic_drv2605` casts the device to `drv2605` specific API or
-// expects it.
-//
-// In behavior_haptic_drv2605.c:
-// drv2605_haptic_config(data->haptic_dev, ...);
-//
-// This function `drv2605_haptic_config` is likely a wrapper that calls a specific API method or
-// checks compatible string? Let's check `zephyr/drivers/haptics/drv2605.h` or source. If
-// `drv2605_haptic_config` is a syscall or inline helper, we might need to implement the syscall
-// handler or if it just calls `haptics_configure`...
-//
-// Actually `drv2605_haptic_config` is NOT standard haptics API. It is DRV2605 specific.
-// We cannot implement a proxy that responds to `drv2605_haptic_config` unless we ARE a drv2605
-// compatible driver or we patch the behavior to use a generic haptics API if possible.
-//
-// However, looking at `behavior_haptic_drv2605.c`:
-// It includes `zephyr/drivers/haptics/drv2605.h`.
-// It calls `drv2605_haptic_config`.
-//
-// If we want our proxy to work, we might need to change the behavior to be potentially more
-// generic, OR we rely on `drv2605_haptic_config` possibly checking the driver API?
-//
-// If `drv2605_haptic_config` is implemented in the zephyr driver subsystem, it likely fails if the
-// driver is not `ti,drv2605`.
-//
-// Let's assume for a moment we can't change the behavior too much (or we can, it is in `modules/`).
-//
-// BETTER PLAN:
-// Modify `behavior_haptic_drv2605.c` to support a generic haptic device if the device is not
-// `ti,drv2605`. OR Make our proxy compatible `ti,drv2605` ?? No, that's messy.
-//
-// Valid approach:
-// 1. Modify `behavior_haptic_drv2605.c` to check if the device is a proxy.
-// 2. OR, better, add a generic path in `behavior_haptic_drv2605.c`.
-//
-// But wait, `drv2605_haptic_config` takes `union drv2605_config_data`.
-// Our proxy can accept this data.
-//
-// Implementation Details:
-// The `haptics_driver_api` has `configure`? Zephyr 3.5+?
-//
-// Let's implement `haptic_output_proxy` as a minimal haptic driver. Not referencing `drv2605`
-// header in strict sense? No, the behavior passes `drv2605_rom_data`.
-//
-// So we must handle `drv2605_haptic_config`.
-//
-// If `drv2605_haptic_config` is just a function in the driver:
-// It usually calls `dev->api->...`?
-//
-// Let's blindly implement a standard haptics driver first.
 
 static int haptic_output_proxy_init(const struct device *dev) {
-    const struct haptic_output_proxy_config *config = dev->config;
-    if (config->output_dev && !device_is_ready(config->output_dev)) {
-        LOG_ERR("Output device not ready");
-        return -ENODEV;
-    }
     return 0;
 }
 
 #define HAPTIC_OUTPUT_PROXY_DEFINE(inst)                                                           \
-    static struct haptic_output_proxy_data data_##inst;                                            \
-    static const struct haptic_output_proxy_config config_##inst = {                               \
-        .output_dev = COND_CODE_1(DT_INST_NODE_HAS_PROP(inst, output_device),                      \
-                                  (DEVICE_DT_GET(DT_INST_PHANDLE(inst, output_device))),           \
-                                  (NULL)),                                                         \
-    };                                                                                             \
-    DEVICE_DT_INST_DEFINE(inst, haptic_output_proxy_init, NULL, &data_##inst, &config_##inst,      \
-                          POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,                           \
-                          &haptic_output_proxy_api);
+    static const struct haptic_output_proxy_config config_##inst;                                  \
+    DEVICE_DT_INST_DEFINE(inst, haptic_output_proxy_init, NULL, NULL, &config_##inst,              \
+                          POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,                        \
+                          NULL);
 
 DT_INST_FOREACH_STATUS_OKAY(HAPTIC_OUTPUT_PROXY_DEFINE)
 
 #endif /* DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT) */
+
